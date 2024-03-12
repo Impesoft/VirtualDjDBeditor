8cb5fda0a3741746a35c025a443a3a90e254b97f
Ward Impe <ward.impe@dotnetlab.eu>
2024-03-12 10:40:28 +0100

    more clean up in code behind

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,8 +5,6 @@ namespace VirtualDjDBeditor
 {
     public partial class MainWindow : Window
     {
-        private MainViewModel ViewModel => DataContext as MainViewModel;
-
         public MainWindow()
         {
             InitializeComponent();
